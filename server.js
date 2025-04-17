4df3c9190ef0a756fd7a7970faa7c8c3a6f056d6
vipparty18 <jancihugo@gmail.com>
2025-04-17 21:38:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const fs = require('fs');
 const app = express();
 const PORT = 3000;
 
+const cooldownFile = 'cooldown.json'; // tu budeme ukladať cooldowny
+
 app.use(express.static(__dirname));
 app.use(express.json());
 
+// Ukladanie odpovedí do answers.log
 app.post('/log', (req, res) => {
   const logEntry = `Otázka ${req.body.question}: ${req.body.answer}\n`;
   fs.appendFile('answers.log', logEntry, err => {
@@ -18,6 +21,34 @@ app.post('/log', (req, res) => {
   });
 });
 
+// Uloženie cooldownu na 24 hodín
+app.post('/saveCooldown', (req, res) => {
+  const ip = req.ip;
+  const unlockTime = Date.now() + 24 * 60 * 60 * 1000; // teraz + 24h
+
+  let cooldowns = {};
+  if (fs.existsSync(cooldownFile)) {
+    cooldowns = JSON.parse(fs.readFileSync(cooldownFile));
+  }
+
+  cooldowns[ip] = unlockTime;
+  fs.writeFileSync(cooldownFile, JSON.stringify(cooldowns));
+  res.send({ success: true });
+});
+
+// Overenie cooldownu
+app.get('/checkCooldown', (req, res) => {
+  const ip = req.ip;
+  let unlockTime = 0;
+
+  if (fs.existsSync(cooldownFile)) {
+    const cooldowns = JSON.parse(fs.readFileSync(cooldownFile));
+    unlockTime = cooldowns[ip] || 0;
+  }
+
+  res.send({ unlockTime });
+});
+
 app.listen(PORT, () => {
-  console.log(`Server beží na http://localhost:${PORT}`);
+  console.log(`🟢 Server beží na http://localhost:${PORT}`);
 });
